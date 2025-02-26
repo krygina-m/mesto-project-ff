0389f75c5c93424aee3d4b133a1eeffa26849496
Maya Krygina <maya.krygina@yandex.ru>
2025-02-26 15:08:44 +0300

    испарвление функции создания и альта

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,21 @@
 // @todo: Темплейт карточки
 // @todo: DOM узлы
 /*const cardTemp = document.querySelector('#card-template').content;
-const addButton = document.querySelector('.profile__add-button');
-const deleteButton = document.querySelector('.card__delete-button');*/
-const placesList = document.querySelector(".places__list");
+const addButton = document.querySelector('.profile__add-button');*/
+const cardsContainer = document.querySelector(".places__list");
 
 // @todo: Функция создания карточки
 
-function addCard({ name, link }) {
+function addCard({ name, link }, deleteCard) {
   const cardTemplate = document.querySelector("#card-template").content;
   const cardElement = cardTemplate
     .querySelector(".places__item")
     .cloneNode(true);
   cardElement.querySelector(".card__title").textContent = name;
   cardElement.querySelector(".card__image").src = link;
-  const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteCard);
+  cardElement.querySelector(".card__image").alt = name;
+  //const deleteButton = cardElement.querySelector(".card__delete-button");
+  cardElement.querySelector(".card__delete-button").onclick = deleteCard;
 
   return cardElement;
 }
@@ -27,4 +27,4 @@ function deleteCard() {
 }
 
 // @todo: Вывести карточки на страницу
-initialCards.map(addCard).forEach((card) => placesList.append(card));
+initialCards.map(card => addCard(card, deleteCard)).forEach((card) => cardsContainer.append(card));
